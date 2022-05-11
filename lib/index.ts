a9d65386cc3035066d1a8b6338736c071c6cb787
Douglas Naphas <douglasnaphas@gmail.com>
2022-05-11 01:43:34 -0400

    Try to deploy an APIGW/Lambda that says the region

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
 import { App, Stack, StackProps, RemovalPolicy, CfnOutput } from "aws-cdk-lib";
 import { aws_s3 as s3 } from "aws-cdk-lib";
+import { aws_lambda as lambda } from "aws-cdk-lib";
+import { aws_apigateway as apigateway } from "aws-cdk-lib";
 
 export interface AppStackProps extends StackProps {
   customProp?: string;
@@ -19,5 +21,21 @@ export class AppStack extends Stack {
     new CfnOutput(this, "BucketName", {
       value: bucket.bucketName,
     });
+    const fn = new lambda.Function(this, "Function", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      environment: {
+        REGION: this.region,
+      },
+      code: lambda.Code.fromInline(
+        `exports.handler = function(event, ctx, cb) {return {statusCode: 200, body: JSON.stringify({"region": process.env.REGION})}}`
+      ),
+    });
+    const lambdaRestApi = new apigateway.LambdaRestApi(this, "API", {
+      handler: fn,
+      proxy: false
+    })
+    const api = 
+    new CfnOutput(this, "APIEndpoint" {value: lambdaRestApi.url})
   }
 }
